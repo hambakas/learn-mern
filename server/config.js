8ab1cdb49d0922e7385ffd76d2f82e8344d2ecd5
hambakas <hambakas@hotmail.com>
2016-03-27 12:51:40 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://54.201.8.155:27017/test',
   port: process.env.PORT || 8000,
 };
 
